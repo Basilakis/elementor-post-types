510c80ea44582f112def448795ca7de44c259836
Basilis Kanonidis <basilis@jailbait.gr>
2017-07-10 21:12:42 +0300

    Update and rename testimonials.php to example-functions.php

---
+++ example-functions.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Get the bootstrap! If using the plugin from wordpress.org, REMOVE THIS!
+ */
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+	require_once dirname( __FILE__ ) . '/CMB2/init.php';
+}
+/**
+ * Conditionally displays a metabox when used as a callback in the 'show_on_cb' cmb2_box parameter
+ *
+ * @param  CMB2 object $cmb CMB2 object.
+ *
+ * @return bool             True if metabox should show
+ */
+function yourprefix_show_if_front_page( $cmb ) {
+	// Don't show this metabox if it's not the front page template.
+	if ( get_option( 'page_on_front' ) !== $cmb->object_id ) {
+		return false;
+	}
+	return true;
+}
+/**
+ * Conditionally displays a field when used as a callback in the 'show_on_cb' field parameter
+ *
+ * @param  CMB2_Field object $field Field object.
+ *
+ * @return bool                     True if metabox should show
+ */
+function yourprefix_hide_if_no_cats( $field ) {
+	// Don't show this field if not in the cats category.
+	if ( ! has_tag( 'cats', $field->object_id ) ) {
+		return false;
+	}
+	return true;
+}
+/**
+ * Conditionally displays a message if the $post_id is 2
+ *
+ * @param  array             $field_args Array of field parameters.
+ * @param  CMB2_Field object $field      Field object.
+ */
+function yourprefix_before_row_if_2( $field_args, $field ) {
+	if ( 2 == $field->object_id ) {
+		echo '<p>Testing <b>"before_row"</b> parameter (on $post_id 2)</p>';
+	} else {
+		echo '<p>Testing <b>"before_row"</b> parameter (<b>NOT</b> on $post_id 2)</p>';
+	}
+}
+?>
